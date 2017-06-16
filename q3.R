76911f6c4358a16747d904f16776d69be8441364
fengkehh <fengkehh@gmail.com>
2017-06-16 13:53:43 -0700

    Finished report.

--- q3.R
+++ q3.R
@@ -1,3 +1,14 @@
 library('FrF2')
 
-design <- FrF2(nruns = 16, nfactors = 10)
+feature_names <- c('Feature 1','Feature 2','Feature 3','Feature 4','Feature 5', 
+                   'Feature 6','Feature 7','Feature 8','Feature 9','Feature 10')
+
+design <- FrF2(nruns = 16, factor.names = feature_names)
+
+design_frame <- data.frame(design)
+
+for (varname in names(design_frame)) {
+    design_frame[,varname] <- factor(design_frame[,varname], levels = c(-1, 1), 
+                                   labels = c('No', 'Yes'))
+}
+
